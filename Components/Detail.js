2b57430c435799309bac375ec1cad4e752598a52
KoffiDMonky <agenor.houessou@hotmail.fr>
2022-11-07 22:32:55 +0100

    tab classement

--- Components/Detail.js
+++ Components/Detail.js
@@ -10,6 +10,7 @@ import {
   StyleSheet,
 } from 'react-native';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
+import Picker from '@ouroboros/react-native-picker';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Accordeon from './Accordeon';
 import Ranking from './Ranking';
@@ -23,6 +24,8 @@ export default Detail = props => {
   //Affiche l'app quand la donnée est chargée
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  let paramPhases = [];
+  let [picker, setPicker] = useState('PHASE PRELIMINAIRE');
 
   //Récupère les données du club
   const getDetail = async () => {
@@ -48,8 +51,14 @@ export default Detail = props => {
   //Tableau d'accordeon contenant le détail des équipes
   const detailsEquipes = [];
 
-  //Tableau d'accordeon contenant le classement
+  //lignes du tableau contenant le classement
   const teamsRanking = [];
+
+  //Filtre d'objet
+  Object.filter = (obj, predicate) =>
+    Object.keys(obj)
+      .filter(key => predicate(obj[key]))
+      .reduce((res, key) => ((res[key] = obj[key]), res), {});
 
   //------------------------------ Code de la TabBar (Début) ------------------------------//
   //Rendu de la tabBar
@@ -74,14 +83,41 @@ export default Detail = props => {
 
   //Contenu du tab 2 : Equipes
   const SecondRoute = () => (
-    <ScrollView style={styles.tabContainer}>{detailsEquipes}</ScrollView>
+    <ScrollView style={styles.tabContainer}>
+      <Picker
+        onChanged={setPicker}
+        options={paramPhases}
+        style={{
+          borderWidth: 1,
+          borderColor: '#a7a7a7',
+          borderRadius: 5,
+          marginVertical: 5,
+          padding: 5,
+        }}
+        value={picker}
+      />
+      {detailsEquipes}
+    </ScrollView>
   );
 
   //Contenu du tab 3 : Classement
   const ThirdRoute = () => (
-    <ScrollView style={styles.tabContainer}>
-      <Text>Classement</Text>
-      {teamsRanking}
+    <ScrollView
+      style={styles.tabContainer}
+      contentContainerStyle={{alignItems: 'center'}}>
+      <Picker
+        onChanged={setPicker}
+        options={paramPhases}
+        style={{
+          borderWidth: 1,
+          borderColor: '#a7a7a7',
+          borderRadius: 5,
+          marginVertical: 5,
+          padding: 5,
+        }}
+        value={picker}
+      />
+      <Ranking teamsRanking={teamsRanking} />
     </ScrollView>
   );
 
@@ -117,18 +153,55 @@ export default Detail = props => {
           />,
         );
       }
+      paramPhases.push({
+        value: phase.phase.phase.libelle,
+        text: phase.phase.phase.libelle,
+      });
+    }
+  }
 
-      //Si l'on est dans le tab equipes et que le détail des équipes n'est pas indefini, on mets l'accordeon dans le tableau détailsEquipes
-      if (index === 1 && phase.detailsEquipes !== undefined) {
-        for (const [i, team] of phase.detailsEquipes.entries())
+  if (phases !== undefined) {
+    const filterPhase = phases.filter(
+      phase => phase.phase.phase.libelle === picker,
+    );
+    //Si l'on est dans le tab equipes et que le détail des équipes n'est pas indefini, on mets l'accordeon dans le tableau détailsEquipes
+    if (index === 1) {
+      for (const [i, phase] of filterPhase.entries()) {
+        for (const [j, team] of phase.detailsEquipes.entries()) {
           detailsEquipes.push(
             <Accordeon
-              key={i}
+              key={j}
               tab={index}
               title={team.club.nom}
               detailsEquipe={team}
             />,
           );
+        }
+      }
+    }
+
+    if (index === 2) {
+      for (const [i, phase] of filterPhase.entries()) {
+
+        // console.log(phase.classements); 
+       const sortRanking = phase.classements.sort((a,b) => a.place - b.place);
+
+        for (const [j, team] of sortRanking.entries()) {
+          teamsRanking.push(
+            <View
+              key={j}
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                borderBottomWidth: 2,
+                paddingVertical: 12,
+              }}>
+              <Text style={{flex: 1}}>N° {team.place}</Text>
+              <Text style={{flex: 3}}>{team.nom}</Text>
+              <Text style={{flex: 1}}>{team.points} pts</Text>
+            </View>,
+          );
+        }
       }
     }
   }
--- Components/Ranking.js
+++ Components/Ranking.js
@@ -1,12 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, Text, View} from 'react-native';
-
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 
 export default Ranking = props => {
-
+  //lignes du tableau contenant le classement
+  const teamsRanking = props.teamsRanking;
 
   return (
-    <View style={{flex: 1, padding: 12}}>
+    <View style={{flex: 1, padding: 12, width: '90%'}}>
+      <View
+        style={{flexDirection: 'row', borderBottomWidth: 2, paddingVertical: 12}}>
+        <Text style={{flex: 1}}>N°</Text>
+        <Text style={{flex: 3}}>Equipe</Text>
+        <Text style={{flex: 1}}>Points</Text>
+      </View>
+      {teamsRanking}
     </View>
   );
 };
